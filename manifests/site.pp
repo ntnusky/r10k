ce4a5283f7742cc7faa1130c7ca44e5dceb118b4
obrestad <eigilo@hig.no>
2015-06-12 10:41:20 +0200

    Removed site.pp from development.

--- manifests/site.pp
+++
@@ -1,6 +0,0 @@
-# Regex stripping off the digits at the end of the hostname. Useful
-# for creating the "groups" used in hieara configurations.
-$hostgroup = regsubst($clientcert, '\d+$', '')
-
-# Use hieara to apply classes to nodes.
-hiera_include('classes')
